3394dd207597badd4701447ae30382b8850cfcbe
ONIYELE ANJOLA E <anji@localhost.localdomain>
2025-05-24 20:13:24 +0100

    Fix: correct type for ProductPage params

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,18 @@
 import { ProductDetail } from "@/components/product-detail";
-import { stripe } from "@/lib/stripe"
+import { stripe } from "@/lib/stripe";
 
-export default async function ProductsPage ({params}: {params: { id: string }; }) {
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ["default_price"],
-    })
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
-    const plainProduct = JSON.parse(JSON.stringify(product))
-    return <ProductDetail product={plainProduct}/>
+export default async function ProductsPage({ params }: Props) {
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"],
+  });
+
+  const plainProduct = JSON.parse(JSON.stringify(product));
+
+  return <ProductDetail product={plainProduct} />;
 }
